57c9eba298ea6cc8826496624d32ada581b6201b
xuhao17 <xuhao17@lenovo.com>
2023-06-20 22:50:33 +0800

    add health rest api

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -1,9 +1,12 @@
 package com.gupao.springbootdemo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @RestController
 public class K8SController {
@@ -12,5 +15,10 @@ public class K8SController {
     @RequestMapping("/k8s")
     public String k8s(){
         return "hello K8s <br/>111222 ";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
     }
 }
